28bd71b4ae72fe9895f7d0dabda7ead5bb096ca4
abmilord <a.milord2507@gmail.com>
2023-11-30 01:20:54 -0500

    Revert "minor changes"
    This reverts commit b3f03747bf07ec2e48390b0090f81878b94924cc.

--- My project/Assets/Scripts/HealthCollectible.cs
+++ My project/Assets/Scripts/HealthCollectible.cs
@@ -8,7 +8,7 @@ public class HealthCollectible : MonoBehaviour
    
    void OnTriggerEnter2D(Collider2D other)
    {
-    RubyController controller = other.GetComponent<RubyController>();
+     RubyController controller = other.GetComponent<>(RubyController);
 
     if(controller != null)
     {
