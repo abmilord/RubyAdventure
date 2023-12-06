7963af2df3cee61d8616b02ce046b5861fccc549
abmilord <a.milord2507@gmail.com>
2023-12-06 01:30:44 -0500

    commit

--- My project/Assets/Scripts/PowerUpEffect.cs
+++ My project/Assets/Scripts/PowerUpEffect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigCogBullet : MonoBehaviour
+public class PowerUpEffect : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
